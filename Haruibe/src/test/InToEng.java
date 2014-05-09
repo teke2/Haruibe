88285d8e775c9edc76266c53517f447d9e1d763d
g12914iy <g12914iy@s30421.tsuda.ac.jp>
2014-05-09 13:08:53 +0900

    仁枝:ドライバー 石井:ナビゲーター 桁ごとに配列にいれて1~19まで正しく英訳を返せるようにした。

--- Haruibe/src/test/InToEng.java
+++ Haruibe/src/test/InToEng.java
@@ -11,18 +11,31 @@ public class InToEng {
 
 	//数値を英訳する変換するメソッド
 	static String translateEng(int n){
-
-		//0~9が入力されたらStringの中の文字列をかえす
-		String[] a ={"zero","one","two","three","four","five","six","seven","eight","nine"};
-		return a[n];
-
-		//読み込んだ数値を１の位から一桁ずつ配列に入れる
 /*
-		int a[]=new int[10];
-		for(int i=0;n/10>0;i++)
+		//0~9が入力されたらStringの中の文字列をかえす
+
+		return a[n];
+*/
+		String[] s1 ={"zero","one","two","three","four","five","six","seven","eight","nine"};
+		String[] s2 ={"ten","eleven","twelve","thirteen","forteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+		//読み込んだ数値の桁数を求める
+		int y=n;
+		int count=0;
+		while(y>0){
+			y/=10;
+			count++;
+		}
+		//読み込んだ数値を１の位から一桁ずつ配列に入れる
+		int a[]=new int[count];
+		for(int i=0;n>0;i++){
 			a[i]=n%10;
+			n=n/10;
+		}
+		if(a.length>1 && a[1]==1) System.out.print(s2[a[0]]);
+		else for(int i=a.length-1;i>=0;i--)
+			System.out.print(s1[a[i]]+" ");
 		String c ="";
 		return c;
-		*/
+		
 	}
 }
