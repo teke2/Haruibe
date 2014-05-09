c822b9fa610b536646cffb8780eb5c630ab33499
g12914iy <g12914iy@s30421.tsuda.ac.jp>
2014-05-09 15:16:06 +0900

    ドライバー:仁枝 ナビゲーター:石井 6桁の整数(0~99万まで)の英訳を返すようにした。

--- Haruibe/src/test/InToEng.java
+++ Haruibe/src/test/InToEng.java
@@ -11,13 +11,13 @@ public class InToEng {
 
 	//”’l‚ğ‰p–ó‚·‚é•ÏŠ·‚·‚éƒƒ\ƒbƒh
 	static String translateEng(int n){
-		//0~9‚ª“ü—Í‚³‚ê‚½‚çString‚Ì’†‚Ì•¶š—ñ‚ğ‚©‚¦‚·
-		String[] s1 ={"","one","two","three","four","five","six","seven","eight","nine"};
-		String[] s2 ={"ten","eleven","twelve","thirteen","forteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-		String[] s3 ={"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+
+	
 		//“Ç‚İ‚ñ‚¾”’l‚ÌŒ…”‚ğ‹‚ß‚é
+
 		int y=n;
-		int x=n;
+		//Œ³X‚Ì”’l‚ª0‚¾‚Á‚½‚çzero‚ğ•Ô‚·
+		if(n==0) return "zero";
 		int count=0;
 		while(y>0){
 			y/=10;
@@ -29,15 +29,31 @@ public class InToEng {
 			a[i]=n%10;
 			n=n/10;
 		}
-		if(x==0) return "zero";
-		//if(a.length==1&&a[0]==0) return "zero";
 		String str="";
-		//‚RŒ…‚²‚Æ‚É“¯‚¶‚æ‚¤‚Èì‹Æ‚ğŒJ‚è•Ô‚µ‚½‚¢,12,000‚Í12ŒÂ‚Ì1000‚Ì‚Æ‚¢‚Á‚½‚æ‚¤‚ÈŠ´‚¶‚Å
-		if(a.length>2) str += s1[a[2]]+" hundred ";
-		if(a.length>1 && a[1]==1){str +=s2[a[0]]; return str;}
-			else if(a.length>1) str +=s3[a[1]]+" ";
-		//•¶š—ñ‚ª‹ó‚¾‚Á‚½‚ç0•Ô‚·
-		str+=s1[a[0]];
+		if((a.length-1)/3==1){
+			str += xyz(a,1) + " thousand ";
+			str += xyz(a,0);
+		}
+		if((a.length-1)/3==0)
+			str += xyz(a,0);
+		
+
 		return str;
 	}
+	 //‚RŒ…‚²‚Æ‚É“¯‚¶‚æ‚¤‚Èì‹Æ‚ğŒJ‚è•Ô‚µ‚½‚¢,12,000‚Í12ŒÂ‚Ì1000‚Ì‚Æ‚¢‚Á‚½‚æ‚¤‚ÈŠ´‚¶‚Å
+	static String xyz(int[] b,int c){
+		String str="";
+		//0~9‚ª“ü—Í‚³‚ê‚½‚çString‚Ì’†‚Ì•¶š—ñ‚ğ‚©‚¦‚·
+		String[] s1 ={"","one","two","three","four","five","six","seven","eight","nine"};
+		String[] s2 ={"ten","eleven","twelve","thirteen","forteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+		String[] s3 ={"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+
+		if(b.length>(2+3*c)) str += s1[b[2+3*c]]+" hundred ";
+		if(b.length>(1+3*c) && b[1+3*c]==1) {str +=s2[b[0+3*c]]; return str;}
+		else if(b.length>(1+3*c)) str +=s3[b[1+3*c]]+" ";
+		str+=s1[b[0+3*c]];
+		return str;
+	}	
+
+
 }
