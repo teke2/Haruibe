fd5d142110be252ab77d05497b82b348c91b209f
g12914iy <g12914iy@s30421.tsuda.ac.jp>
2014-05-09 11:35:44 +0900

    とりあえずリポジトリに置く

---
+++ Haruibe/src/test/InToEng.java
@@ -0,0 +1,16 @@
+package test;
+import java.util.*;
+public class InToEng {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		System.out.println(translateEng(input));
+	}
+
+	//数値を英訳する変換するメソッド
+	static String translateEng(int n){
+		return"";
+	}
+}
